9b5ed2230138ea1281fc5f9e31c66ca6cde3d904
complex-circuit <30806115+complex-circuit@users.noreply.github.com>
2021-09-08 18:02:08 -0400

    Add files via upload

--- vectorthrustvar.ino
+++ vectorthrustvar.ino
@@ -17,13 +17,27 @@ void loop() {int l=pulseIn(5,HIGH,30000);
   if(l>100)
     v=l;
     double V=(v-994)/994.0;
-    //V is between 0 and 1. 1 is centered thrust, and 0 is vectored thrust.
+    if(V>1)
+      V=1;
+    if(V<0)
+      V=0;
+  
     int elev=(pulseIn(4,HIGH)-994)*180*V/994;
     int rudd=(pulseIn(11,HIGH)-994)*180*V/994;
     elev+=90*(1-V);
     rudd+=90*(1-V);
-    elevVec.write(elev/2+45);
-    ruddVec.write(rudd/2+45);
+    //V is between 0 and 1. 1 is centered thrust, and 0 is vectored thrust.
+    //The next 4 one-line if statements are safeties because the elevator was glitching
+    //and would have resulted in the plane being unable to fly safely.
+    elev=elev/2+45;
+    rudd=rudd/2+45;
+    if(elev>135)elev=135;
+    if(elev< 45)elev= 45;
+    if(rudd>135)rudd=135;
+    if(rudd< 45)rudd= 45;
+    
+    elevVec.write(elev);
+    ruddVec.write(rudd);
  
   
 
